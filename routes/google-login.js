be65aa0e6ed899a6ee3455bb30df17e5a6ae0eba
Bochen <lee70468@gmail.com>
2024-04-05 02:35:16 +0800

    準備打掉重練

--- routes/google-login.js
+++ routes/google-login.js
@@ -3,6 +3,10 @@ import express from 'express'
 import { OAuth2Client } from 'google-auth-library'
 import sequelize from '#configs/db.js'
 import 'dotenv/config.js'
+import jsonwebtoken from 'jsonwebtoken'
+
+// 定義安全的私鑰字串
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 
 const { Member } = sequelize.models
 const router = express.Router()
@@ -15,12 +19,14 @@ const oAuth2Client = new OAuth2Client(
 
 router.post('/', async (req, res) => {
   const { tokens } = await oAuth2Client.getToken(req.body.code) // exchange code for tokens
-  const { id_token } = tokens
+  const { id_token, access_token } = tokens
   const ticket = await oAuth2Client.verifyIdToken({
     idToken: id_token,
     audience: process.env.CLIENT_ID,
   })
   const { sub: google_uid, email, name: displayName } = ticket.getPayload()
+
+  console.log('access_token', access_token)
 
   const total = await Member.count({
     where: {
@@ -64,7 +70,29 @@ router.post('/', async (req, res) => {
     }
   }
 
-  res.json(returnUser)
+  // 產生存取令牌(access token)，其中包含會員資料
+  const accessToken = jsonwebtoken.sign(returnUser, accessTokenSecret, {
+    expiresIn: '3d',
+  })
+
+  // 將 access token 存儲在 httpOnly cookie 中
+  // 將 access token 存儲在 httpOnly cookie 中
+  res.cookie('accessToken', accessToken, {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true,
+  })
+
+  // 傳送access token回應(react可以儲存在state中使用)
+  // 延遲 1 秒後再結束 response
+  setTimeout(() => {
+    return res.json({
+      status: 'success',
+      data: {
+        accessToken,
+      },
+    })
+  }, 1000)
 })
 
 router.post('/auth/google/refresh-token', async (req, res) => {
