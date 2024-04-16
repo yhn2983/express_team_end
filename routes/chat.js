393eef6683d4df9944e5ce277034923fb99e5c7e
Bochen <lee70468@gmail.com>
2024-04-17 04:49:40 +0800

    聊天室對話功能完成

--- routes/chat.js
+++ routes/chat.js
@@ -6,6 +6,7 @@ import { Op } from 'sequelize'
 // 存取`.env`設定檔案使用
 import 'dotenv/config.js'
 const { Room } = sequelize.models
+const { Message } = sequelize.models
 
 router.post('/', (req, res) => {
   const io = req.app.io
@@ -37,6 +38,60 @@ router.post('/rooms', async (req, res) => {
     res.json({ status: 'success', data: room })
   } catch (error) {
     res.json({ status: 'error', message: error.message })
+  }
+})
+
+router.post('/postmessage', async (req, res) => {
+  try {
+    // 從請求體中獲取訊息資訊
+    const { text, name, id, socketID, connectionState } = req.body
+
+    // 創建新的訊息
+    const message = await Message.create({
+      room_id: connectionState.roomId,
+      sender_id: connectionState.userId,
+      content: text,
+    })
+
+    // 回應請求
+    res.json({ status: 'success', data: message })
+  } catch (error) {
+    // 處理錯誤
+    res.json({ status: 'error', message: error.message })
+  }
+})
+
+router.post('/getmessages', async (req, res) => {
+  try {
+    // 從請求體中獲取聊天室 ID
+    const { roomId } = req.body
+
+    // 驗證 room_id
+    if (!roomId) {
+      return res
+        .status(400)
+        .json({ status: 'error', message: 'room_id is required' })
+    }
+    const room_id = roomId
+
+    // 獲取聊天室中的所有訊息，並按照時間順序排序
+    const roomMessages = await Message.findAll({
+      where: { room_id },
+      order: [['created_at', 'ASC']],
+    })
+
+    // 如果沒有找到訊息，回傳一個特定的響應
+    if (roomMessages.length === 0) {
+      return res.json({
+        status: 'success',
+        message: 'No messages found',
+        data: [],
+      })
+    }
+
+    res.json({ status: 'success', data: roomMessages })
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message })
   }
 })
 
