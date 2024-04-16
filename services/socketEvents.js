d2cc027d7dfac2981dc58b91f0bb442546efd96f
Bochen <lee70468@gmail.com>
2024-04-16 23:41:30 +0800

    聊天室暫存

--- services/socketEvents.js
+++ services/socketEvents.js
@@ -5,6 +5,17 @@ function socketEvents(io) {
   io.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} 用戶已經連接!`) // 當新的用戶連接時，打印出他的 ID
 
+    socket.on('newUser', (data) => {
+      const { connectionState } = data
+      console.log(data)
+      const roomId = connectionState.roomId
+      // 將用戶加入到對應的房間
+      socket.join(`room${roomId}`)
+      console.log(`User has joined room ${roomId}`)
+      // 打印出 socket 目前加入的所有房間
+      console.log(socket.rooms)
+    })
+
     socket.on('message', (data) => {
       console.log(data) // 打印出收到的消息
       io.emit('messageResponse', data) // 將收到的消息發送給所有用戶
@@ -12,10 +23,10 @@ function socketEvents(io) {
 
     socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data)) // 當用戶正在輸入時，通知其他用戶
 
-    socket.on('newUser', (data) => {
-      chatUser.push(data) // 將新用戶添加到用戶列表
-      io.emit('newUserResponse', chatUser) // 將更新後的用戶列表發送給所有用戶
-    })
+    // socket.on('newUser', (data) => {
+    //   chatUser.push(data) // 將新用戶添加到用戶列表
+    //   io.emit('newUserResponse', chatUser) // 將更新後的用戶列表發送給所有用戶
+    // })
 
     socket.on('disconnect', () => {
       console.log('🔥: 一個用戶已經斷開連接') // 當用戶斷開連接時，打印出消息
